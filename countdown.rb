ff1408a8bf6e36843cd7a984233b29a15abd654d
Indu Ramesh <induramesh@gmail.com>
2019-07-15 08:46:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,6 +1,9 @@
 #write your code here
 
 def countdown(num)
-  while num > 0 do
-    puts 
+  while num >= 0 do
+    puts "#{number} SECOND(S)!"
+    num = num - 1
+  end 
+  return "HAPPY NEW YEAR"
 end
